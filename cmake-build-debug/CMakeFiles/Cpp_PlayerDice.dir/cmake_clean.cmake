b1c1f77b8190695a81e1b80fa0beade0b7c9bc48
Chris <christopher.diktakis@gmail.com>
2019-10-31 22:18:45 -0400

    Update Player and Dice classes

---
+++ cmake-build-debug/CMakeFiles/Cpp_PlayerDice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cpp_PlayerDice.dir/Dice.cpp.o"
+  "CMakeFiles/Cpp_PlayerDice.dir/Player.cpp.o"
+  "Cpp_PlayerDice.pdb"
+  "Cpp_PlayerDice"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cpp_PlayerDice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
