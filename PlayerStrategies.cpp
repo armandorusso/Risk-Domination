6a366d3fc3b7471a2a031653922ddc3ce7ffc362
Chris <christopher.diktakis@gmail.com>
2019-11-11 14:09:51 -0500

    Update Strategy files.

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -3,20 +3,24 @@
 
 using namespace std;
 
-//Reinforces strongest country. Always attacks with it until unable
+
+
+
+
+//Concrete strategy that implements an aggressive player.
+//Reinforces strongest country. Always attacks with it until unable.
 void AggressivePlayer::execute() {
     cout << "Aggressive Player: Execute()" << endl;
 }
 
+//Concrete strategy that implements an interactive player.
 //Human interacts with it.
 void HumanPlayer::execute() {
     cout << "Human Player: Execute()" << endl;
 }
 
-
-//Reinforces weakest countries
-//Never atacks
-//Fortifies in order to move armies to weaker countries.
+//Concrete strategy that implements an benevolent player.
+//Reinforces weakest countries. Never atacks. Fortifies in order to move armies to weaker countries.
 void BenevolentPlayer::execute() {
     cout << "Benevolent Player: Execute()" << endl;
 }
