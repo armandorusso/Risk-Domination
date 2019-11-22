5ed1609993aa9dcfd3853164bb285e963a540b63
Chris <christopher.diktakis@gmail.com>
2019-11-21 21:48:51 -0500

    change cmake

---
+++ cmake-build-debug/CMakeFiles/assignment4Base.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/Adapter.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/Adapter.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/AdapterDriver.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/AdapterDriver.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/Cards.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/Cards.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/ConquestLoader.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/ConquestLoader.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/Dice.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/Dice.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/GameEngine.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/GameEngine.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/GameEngine2.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/GameEngine2.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/GameEngineDriver2.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/GameEngineDriver2.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/GameObservers.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/GameObservers.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/Map.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/Map.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/MapLoader.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/MapLoader.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/Player.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/Player.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/PlayerStrategies.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/PlayerStrategies.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/ReinforcementDriver.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/ReinforcementDriver.cpp.o"
+  "/Users/christopherdiktakis/Desktop/Assignment4_345/a3_345.cpp" "/Users/christopherdiktakis/Desktop/Assignment4_345/cmake-build-debug/CMakeFiles/assignment4Base.dir/a3_345.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/assignment4Base.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assignment4Base.dir/a3_345.cpp.o"
+  "CMakeFiles/assignment4Base.dir/Dice.cpp.o"
+  "CMakeFiles/assignment4Base.dir/Player.cpp.o"
+  "CMakeFiles/assignment4Base.dir/Map.cpp.o"
+  "CMakeFiles/assignment4Base.dir/Cards.cpp.o"
+  "CMakeFiles/assignment4Base.dir/GameEngine.cpp.o"
+  "CMakeFiles/assignment4Base.dir/GameEngine2.cpp.o"
+  "CMakeFiles/assignment4Base.dir/ReinforcementDriver.cpp.o"
+  "CMakeFiles/assignment4Base.dir/PlayerStrategies.cpp.o"
+  "CMakeFiles/assignment4Base.dir/MapLoader.cpp.o"
+  "CMakeFiles/assignment4Base.dir/GameEngineDriver2.cpp.o"
+  "CMakeFiles/assignment4Base.dir/GameObservers.cpp.o"
+  "CMakeFiles/assignment4Base.dir/Adapter.cpp.o"
+  "CMakeFiles/assignment4Base.dir/AdapterDriver.cpp.o"
+  "CMakeFiles/assignment4Base.dir/ConquestLoader.cpp.o"
+  "assignment4Base.pdb"
+  "assignment4Base"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assignment4Base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
